b7f49aec3ad34c6a9b37a35cc9be04e31af83735
Alok Patel <alokpatel41001@gmail.com>
2026-04-20 11:24:34 +0530

    first commit

--- backend/src/routes/invoice.routes.js
+++ backend/src/routes/invoice.routes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import {
     generateInvoice, getInvoices, getInvoiceById, getMyInvoices, markInvoicePaid, verifyPayment, cancelInvoice
-} from "../controllers/invoice.controller"
+} from "../controllers/invoice.controller.js"
 
 
 const router = express.Router();
